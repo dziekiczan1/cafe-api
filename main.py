108a999b504ac64a980c08cfca33edcbbe6a8e3f
dzekiczan1 <p.rzadkowolski@gmail.com>
2025-08-24 18:56:09 +0200

    add option to remove item

--- main.py
+++ main.py
@@ -115,6 +115,20 @@ def patch_new_price(cafe_id):
         return jsonify(response={"success": "Successfully updated the price."}), 200
 
 # HTTP DELETE - Delete Record
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "TopSecretAPIKey":
+        try:
+            cafe = db.get(Cafe, cafe_id)
+        except AttributeError:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+        else:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
 
 
 if __name__ == '__main__':
